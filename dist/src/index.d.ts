b8c9b55edd746b3d750e2553cc52d0c4dcd98888
Guillaume Briday <guillaumebriday@gmail.com>
2020-06-24 00:47:19 +0200

    wip

---
+++ dist/src/index.d.ts
@@ -0,0 +1,8 @@
+import './types/vuex-orm';
+import Components from './contracts/Components';
+import Options from './contracts/Options';
+export { Options };
+declare const _default: {
+    install(components: Components, installOptions: Options): void;
+};
+export default _default;
